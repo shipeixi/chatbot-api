43ec4edc9651e64706da0129d4c12d0f5553cc2b
shipeixi <shipeixi22@163.com>
2023-04-10 17:40:38 +0800

    chatbot

---
+++ chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain.java
@@ -0,0 +1,4 @@
+package cn.bugstack.chatbot.api;
+
+public class domain {
+}
---
+++ chatbot-api-infrastructure/src/main/java/cn/bugstack/chatbot/api/AppApplication.java
@@ -0,0 +1,7 @@
+package cn.bugstack.chatbot.api;
+
+public class AppApplication {
+    public static void main(String[] args){
+//        SpringApplication.
+    }
+}
